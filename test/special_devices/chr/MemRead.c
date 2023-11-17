edd0b88a1ec4b802aaf4a57ed93801c64cb8c6ef
Piyuyang <pi_yuyang@163.com>
2023-11-17 09:56:24 +0800

    chr devices test

---
+++ test/special_devices/chr/MemRead.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+    int fd = 0;
+    int dst = 0;
+
+    /*打开设备文件*/
+    fd = open("/dev/memdev0", O_RDWR);
+
+    lseek(fd, 2, SEEK_SET);
+
+    /*写入数据*/
+    read(fd, &dst, sizeof(int));
+
+    printf("dst0 is %d\n", dst);
+
+    /*关闭设备*/
+    close(fd);
+
+    /*打开设备文件*/
+    fd = open("/dev/memdev1", O_RDWR);
+
+    lseek(fd, 3, SEEK_SET);
+    /*写入数据*/
+    read(fd, &dst, sizeof(int));
+
+    printf("dst1 is %d\n", dst);
+
+    /*关闭设备*/
+    close(fd);
+
+    return 0;
+}
