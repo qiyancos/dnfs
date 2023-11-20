1b3bb382e4a9e2bbd033c6b1b85962e3c602a83f
jiaoyuewansui <3059497228@qq.com>
2023-11-20 11:29:59 +0800

    add auto test

--- test/test.sh
+++ test/test.sh
@@ -26,14 +26,22 @@ function test_cd() {
 
 # test ls
 function test_ls() {
-  print "Test ls,the file list will not be displayed unless the number of ls is set to 1"
-  read -p "Please enter the number of ls tests: " test_num
+  if [ $mode_flag -eq 0 ];
+  then
+     print "Test ls,the file list will not be displayed unless the number of ls is set to 1"
+     read -p "Please enter the number of ls tests: " test_num
+  else
+	  print "Test ls 50 times"
+	  test_num=50
+  fi
+
   for(( i=1; i<=$test_num; i++ ))
   do
 	  print "    the $i times ls test"
 	  ls >> /dev/null
 	  if [ $? -ne 0 ];then
 		  print "Test ls failed" $split_str
+		  exit
 	  fi
   done
 
@@ -47,8 +55,15 @@ function test_ls() {
 #test touch 
 function test_touch() {
    print "Test touch file"
-   read -p "Please enter the name of the sequence file to create: " file_name
-   read -p "Please enter the number of the sequence file to create: " file_number
+   if [ $mode_flag -eq 0 ];
+   then
+           read -p "Please enter the name of the sequence file to create: " file_name
+           read -p "Please enter the number of the sequence file to create: " file_number
+   else
+	   print "Will create 100 files"
+	   file_name="test"
+	   file_number=100
+   fi
    for(( i=1; i<=$file_number; i++ ))
    do
 	   print "    Touch file  $file_name$i"
@@ -59,15 +74,22 @@ function test_touch() {
 	   fi
    done
    print "Successfully tested touch the result is:" 
-   ls -lh
+   ls -lh | grep $file_name
    print $split_str
 }
 
 #test mkdir
 function test_mkdir(){
    print "Test make dir"
-   read -p "Please enter the name of the sequence dir to create: " dir_name
-   read -p "Please enter the number of the sequence dir to create: " dir_number
+   if [ $mode_flag -eq 0 ];
+   then
+           read -p "Please enter the name of the sequence dir to create: " dir_name
+           read -p "Please enter the number of the sequence dir to create: " dir_number
+   else
+	   print "Will create 100 directory"
+	   dir_name="test_dir"
+	   dir_number=100
+   fi
    for(( i=1; i<=$dir_number; i++ ))
    do
            print "    Make dir  $dir_name$i"
@@ -78,7 +100,7 @@ function test_mkdir(){
            fi
    done
    print "Successfully tested make dir the result is:" 
-   ls -lh
+   ls -lh | grep $dir_name
    print $split_str
 
 }
@@ -86,8 +108,14 @@ function test_mkdir(){
 #test remove
 function test_remove(){
    print "Test remove"
-   read -p "Please choose to delete individually or delete all,input 1 means all ,other means select one to delete: " delete_mode
-   if [ $delete_mode -eq "1" ];
+   if [ $mode_flag -eq 0 ];
+   then
+           read -p "Please choose to delete individually or delete all,input 1 means all ,other means select one to delete: " delete_mode
+   else
+	   print "Will delete all files in ${mount_path[0]}"
+	   delete_mode=1
+   fi
+   if [ $delete_mode -eq 1 ];
    then
 	   print "All files listed below will be deleted"
 	   ls
@@ -128,52 +156,88 @@ function test_remove(){
 
 }
 
-#test link
-function test_link(){
-	print "Test link"
-	read -p "Please choose to create a soft link or a hard link,input 1 means soft link, other means hard link: " link_mode
-	read -p "Please enter the link source path: " src_path
-	read -p "Please enter the link target name: " target_path
-	read -p "Please enter the number of links to create: " link_num
+#test soft link
+function test_soft_link(){
+	print "Test soft link"
+	if [ $mode_flag -eq 0 ];
+	then
+		read -p "Please enter the soft link source path: " src_path
+		read -p "Please enter the soft link target name: " target_path
+		read -p "Please enter the number of soft links to create: " link_num
+	else
+		print "Will create 50 soft link on test_dir1"
+		src_path=test_dir1
+		target_path=soft_l
+		link_num=50
+	fi
 	if [ -e $src_path ];
 	then
-		if [ $link_mode -eq 1 ];
-		then
-			for (( i=1; i<=$link_num; i++ ))
-			do
-				print "Create soft link $target_path$i"
-               		        ln -sf $src_path $target_path$i
-				if [ $? -ne 0 ];
-				then
-					print "Failed to create soft link $target_path$i" $split_str
-					exit
-				fi
-			done
-		else
-			for (( i=1; i<=$link_num; i++ ))
-			do
-				print "Create hard link $target_path$i"
-				ln -f $src_path $target_path$i
-				if [ $? -ne 0 ];
-				then
-					print "Failed to create hard link $target_path$i" $split_str
-					exit
-				fi
-			done
-		fi
+		for (( i=1; i<=$link_num; i++ ))
+		do
+			print "Create soft link $target_path$i"
+             		ln -sf $src_path $target_path$i
+			if [ $? -ne 0 ];
+			then
+				print "Failed to create soft link $target_path$i" $split_str
+				exit
+			fi
+		done
 	else
-		print "Link source path $src_path not exist"
+		print "Link source path $src_path not exist" $split_str
+		return
 	fi
-	print "Successfully tested link the result is:"
-	ls -lh
+	print "Successfully tested soft link the result is:"
+	ls -lh | grep $target_path
 	print $split_str
+}
+
+#test hard link
+function test_hard_link(){
+        print "Test hard link"
+        if [ $mode_flag -eq 0 ];
+        then
+                read -p "Please enter the hard link source path,directory not allowed to set hard link: " src_path
+                read -p "Please enter the hard link target name: " target_path
+                read -p "Please enter the number of hard links to create: " link_num
+        else
+                print "Will create 50 hard link on test_dir1"
+                src_path=test2
+                target_path=hard_l
+                link_num=50
+	fi
+        if [ -e $src_path ];
+        then
+		for (( i=1; i<=$link_num; i++ ))
+		do
+			print "Create hard link $target_path$i"
+			ln -f $src_path $target_path$i
+			if [ $? -ne 0 ];
+			then
+				print "Failed to create hard link $target_path$i" $split_str
+				exit
+			fi
+		done
+        else
+                print "Link source path $src_path not exist" $split_str
+                return
+        fi
+        print "Successfully tested hard link the result is:"
+        ls -lh | grep $target_path
+        print $split_str
 }
 
 #test mv
 function test_mv(){
 	print "Test mv"
-        read -p "Please enter the mv source path: " src_path
-        read -p "Please enter the mv target name: " target_path
+	if [ $mode_flag -eq 0 ];
+	then
+		read -p "Please enter the mv source path: " src_path
+		read -p "Please enter the mv target name: " target_path
+	else
+		print "Will mv test_dir3 to test_mv"
+		src_path="test_dir3"
+		target_path="test_mv"
+	fi
 	if [ -e $src_path ];
 	then
 		print "Mv $src_path to $target_path"
@@ -184,28 +248,38 @@ function test_mv(){
 			exit
 		fi
 	else
-		print "Mv source path $src_path not exist"
-	fi
-	read -p "Please enter the cp source path: " src_path
-	ls
+		print "Mv source path $src_path not exist" $split_str
+		return
+	fi 
+	print "Successfully tested cp the result is:"
+	ls | grep $target_path
 	print $split_str
 }
 
 #test cp
 function test_cp(){
-        print "Test cp,the source path will be copied directly to the current path"
-        read -p "Please enter the cp source path: " src_path
+        print "Test cp,the target path will be copied directly to the current path"
+	if [ $mode_flag -eq 0 ];
+	then
+		read -p "Please enter the cp source path: " src_path
+		read -p "Please enter the cp target path: " target_path
+	else
+		print "Will cp test_dir4 to test_cp"
+                src_path="test_dir4"
+                target_path="test_cp"
+	fi		
        	if [ -e $src_path ];
         then
                 print "Cp $src_path to `pwd`"
-                cp -rf  $src_path ./
+                cp -rf  $src_path $target_path
                 if [ $? -ne 0 ];
                 then
-                        print "Failed to cp $src_path to $pwd_path" $split_str
+                        print "Failed to cp $src_path to $target_path" $split_str
                         exit
                 fi
         else
-                print "Cp source path $src_path not exist"
+                print "Cp source path $src_path not exist" $split_str
+		return
         fi
         print "Successfully tested cp the result is:"
         ls -lh
@@ -216,7 +290,7 @@ function test_cp(){
 #test vim 
 function test_vim(){
 	print "Test vim"
-	read -p "Please enter the vim file path: " src_path
+	read -p "Please enter the vim file name: " src_path
 	print "Vim $src_path"
         vi $src_path
 	if [ $? -ne 0 ];
@@ -225,25 +299,127 @@ function test_vim(){
 		exit
 	fi
 	print "Successfully tested vim"
+	ls -lh | grep $src_path
         print $split_str
+}
+
+#test chmod
+function test_chmod(){
+	print "Test chmod"
+	ls
+	if [ $mode_flag -eq 0 ];
+	then
+		read -p "Please select the file name to be chmod: " src_path
+		read -p "Please enter the permission to be change: " change_mode
+	else
+		print "Will set file test1 mode 777"
+		src_path="test1"
+		change_mode=777
+	fi
+	if [ -e $src_path ];
+	then
+		print "Change $src_path permission to $change_mode"
+		chmod $change_mode $src_path
+		if [ $? -ne 0 ];
+		then
+			print "Failed to change $src_path permission to $change_mode" $split_str
+			exit
+		fi
+	else
+		print "Chmod file $src_path not exit" $split_str
+		return
+	fi
+	print "Successfully tested chmod the result is:"
+	ls -lh | grep $src_path
+	print $split_str
+
+}
+
+#concurrency test
+function test_concurrency(){
+	print "Concurrency test"
+	test_file="concurrency.txt"
+	if [ ${#mount_path[*]} -ge 2 ];
+	then
+		for (( i=0; i<${#mount_path[*]}; i++))
+		do
+			test_cd ${mount_path[$1]}
+			if [ -e $test_file ];
+			then
+				print "Test file is $test_file"
+			else
+                                print "Create test file $test_file"
+				touch $test_file
+				if [ $? -ne 0 ];
+				then
+					print "Failed to create test file $test_file" $split_str
+					test_cd ${mount_path[0]}
+					exit
+				fi
+			fi
+			{
+				print "Three thousand data will be written to ${mount_path[$i]}/$test_file"
+				for (( j=1; j<3000; i++ ))
+				do
+					echo $j >> "concurrency.txt"
+					if [ $? -ne 0 ];
+					then
+						print "Failed to write data to ${mount_path[$i]}/$test_file"
+						test_cd ${mount_path[0]}
+						exit
+					fi
+			 	done
+			}&
+		done
+
+		wait
+
+	else
+		print "Concurrency test requires at least two mounting paths for testing" $split_str
+	        return
+	fi	
+	print "Successfully tested concurrency"
+	test_cd ${mount_path[0]}
+	print $split_str
 }
 
 print "Start test nfs"
 print "!!!!!!!Any method test will exit when an error is encountered!!!!!!" $split_str
 
-#judge arg num must gt 0
-if [ $# -gt 0 ];
-then
-        print "Mount path is $1" $split_str
+#judge arg num must gt 2 the first is test mode manual or auto next are mount paths
+if [ $# -ge 2 ];
+then   
+        #first get mount path
+        index=-1
+	mount_path=$@
+        for i in $@
+	do
+		if [ $index -eq -1 ];
+		then
+			let index++
+			continue
+		fi
+		mount_path[$index]=$i
+		let index++
+	done
+	
+	#judge path exist
+	for i in ${mount_path[@]}
+	do
+		if [ -e $i ];
+		then
+			continue
+		else
+			print "Mount path $i not exist" $split_str
+		fi
+	done
 
-	#judge dectory
-        print "Determine whether the mounting directory exists"
-	if [ -d $1 ];
+	mode_flag=$1
+
+	if [ $mode_flag -eq 0 ];
 	then
-		print "Mount path $1 exist" $split_str
-
-		#enter mount path
-                test_cd $1
+		print "Select manual test mode" $split_str
+                test_cd ${mount_path[0]}
 
 
                 while [ 1 -eq 1 ]
@@ -253,10 +429,12 @@ then
 			       2 touch file\n
 			       3 make dir\n
 			       4 remove\n
-			       5 link\n
-			       6 mv\n
-			       7 cp\n
-			       8 vim\n
+			       5 soft_link\n
+			       6 hard_link\n
+			       7 mv\n
+			       8 cp\n
+			       9 vim\n
+			       10 chmod\n
 			       other exit\n";
 			read -p "You select is: " select
 			case $select in "1")
@@ -276,20 +454,28 @@ then
                                 test_remove
                                 ;;
 			"5")    
-				#test link
-				test_link
+				#test soft link
+				test_soft_link
 				;;
 			"6")
+				#test hard link
+				test_hard_link
+				;;
+			"7")
 				#test mv
 				test_mv
 				;;
-			"7")    
+			"8")    
 				#test cp
 				test_cp
 				;;
-			"8")    
+			"9")    
 				#test vim
 				test_vim
+				;;
+			"10")   
+				#test chmod
+				test_chmod
 				;;
 			*)
 				print "Exit ,thank you" $split_str
@@ -297,10 +483,42 @@ then
 			esac
 		done
 	else
-		print "Mount path $1 not exist" $split_str
-		
+		print "Select automatic test mode" $split_str
+		test_cd ${mount_path[0]}
+	
+		#first clear dir
+		test_remove
+
+		#create file
+		test_touch
+
+		#create dir
+		test_mkdir
+
+		#test ls
+		test_ls
+
+		#soft link
+		test_soft_link
+
+		#hard_link
+		test_hard_link
+
+		#test mv
+		test_mv
+
+		#test cp
+		test_cp
+
+		#test chmod
+		test_chmod
+
+		#test rm
+		test_remove
+
 	fi
+
 else
-	print "Need nfs mount path" $split_str
+	print "Please enter test mode and at least one mount path" $split_str
 fi
 	
