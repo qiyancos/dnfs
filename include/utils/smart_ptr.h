693ff2800552bf9ddfcd742eb6bc9962a9dfcb54
jiaoyuewansui <3059497228@qq.com>
2023-11-28 20:04:28 +0800

    smart ptr have delete flag

--- include/utils/smart_ptr.h
+++ include/utils/smart_ptr.h
@@ -16,7 +16,7 @@
 #define DNFSD_SMART_PTR_H
 
 #include <mutex>
-
+#include <map>
 template<typename T>
 class SmartPtr {
 private:
@@ -26,6 +26,13 @@ private:
     int *count;
     /*计数锁*/
     std::mutex *count_mutex;
+    /*计数变化标志，防止只初始化了一次，导致没有释放内存*/
+    bool *count_change;
+    /*已经删除标志*/
+    bool *is_delete;
+    /*在初始化的数据后第一次析构啥都不做*/
+    bool *is_build;
+
 public:
     /*构造函数*/
     explicit SmartPtr(T *ptr);
@@ -54,6 +61,9 @@ public:
 
     /*返回引用计数*/
     int &use_count();
+
+    /*返回是否已经删除数据*/
+    bool get_delete();
 
     /*重载* */
     T &operator*() {
@@ -85,19 +95,26 @@ public:
 /*构造函数*/
 template<typename T>
 SmartPtr<T>::SmartPtr(T *ptr) {
+    printf("%p,调用构造函数\n", ptr);
     this->ptr = ptr;
-    count = new int(1);
+    count = new int(0);
     count_mutex = new std::mutex;
+    count_change = new bool(false);
+    is_delete = new bool(false);
+    is_build = new bool(true);
 }
 
 /*拷贝构造函数*/
 template<typename T>
 SmartPtr<T>::SmartPtr(const SmartPtr<T> &cp_ptr) {
-    printf("%s\n", "调用拷贝构造");
     /*复制参数*/
     ptr = cp_ptr.ptr;
     count = cp_ptr.count;
     count_mutex = cp_ptr.count_mutex;
+    count_change = cp_ptr.count_change;
+    is_delete = cp_ptr.is_delete;
+    is_build = cp_ptr.is_build;
+    printf("%p,%s\n", ptr, "调用拷贝构造");
     /*计数加1*/
     add_count();
 }
@@ -105,9 +122,12 @@ SmartPtr<T>::SmartPtr(const SmartPtr<T> &cp_ptr) {
 /*计数器加1*/
 template<typename T>
 void SmartPtr<T>::add_count() {
+    /*count数量变更标志*/
+    *count_change = true;
     /*加锁计数*/
     std::unique_lock<std::mutex> count_l(*count_mutex);
     (*count)++;
+    printf("%p,计数器为 %d\n", ptr, *count);
 }
 
 /*重载=，只有当两个ptr不相等时计数加1*/
@@ -116,25 +136,51 @@ int &SmartPtr<T>::use_count() {
     return *count;
 }
 
+/*返回是否已经删除数据*/
+template<typename T>
+bool SmartPtr<T>::get_delete() {
+    return *is_delete;
+}
+
 /*释放内存*/
 template<typename T>
 void SmartPtr<T>::realse() {
-    printf("%s\n", "调用解析");
-    /*设置删除标志*/
-    bool delete_flag = false;
-    /*如果需要计数为0*/
-    if (--(*count) == 1) {
-        printf("%s\n", "真正释放");
+    printf("%p,%s\n", ptr, "调用解析");
+    {
         std::unique_lock<std::mutex> count_l(*count_mutex);
-        /*释放内存*/
-        delete count;
-        delete ptr;
-        ptr = nullptr;
-        count = nullptr;
-        delete_flag = true;
+        /*如果需要计数为0*/
+        if (--(*count) == 1 or !*count_change) {
+            printf("%p,%s\n", ptr, "真正释放");
+            /*释放计数内存*/
+            delete count;
+            count = nullptr;
+
+            /*释放内容内存*/
+            delete ptr;
+            ptr = nullptr;
+
+            /*设置删除标志*/
+            *is_delete = true;
+            return;
+        }
+        if (count != nullptr) {
+            printf("%p,现在的count %d\n", ptr, *count);
+        }
     }
-    /*删除锁空间*/
-    if (delete_flag) {
+    /*说明这是最后一个指针，进行删除剩余空间操作*/
+    if (*is_delete) {
+        {
+            printf("彻底删除\n");
+            std::unique_lock<std::mutex> count_l(*count_mutex);
+            /*归还删除标志位空间*/
+            delete is_delete;
+            is_delete = nullptr;
+
+            /*归还计数变化空间*/
+            delete count_change;
+            count_change = nullptr;
+        }
+        printf("删除锁空间\n");
         delete count_mutex;
         count_mutex = nullptr;
     }
@@ -144,8 +190,11 @@ void SmartPtr<T>::realse() {
 /*析构函数*/
 template<typename T>
 SmartPtr<T>::~SmartPtr() {
-    printf("%s\n", "调用析构");
-    realse();
+    printf("%p,%s\n", ptr, "调用析构");
+    if (!*is_build) {
+        realse();
+    }
+    *is_build = false;
 }
 
 
--- test/cache/main.cpp
+++ test/cache/main.cpp
@@ -18,11 +18,61 @@
 #include <thread>
 #include <ctime>
 #include <map>
-#include <ptr
 #include "utils/smart_ptr.h"
 #include <memory>
 
+
 using namespace std;
+
+class PoolTest {
+    map<int, SmartPtr<string>> map_test;
+public:
+    /*得到数据*/
+    SmartPtr<string> get(const int &t);
+
+    /*存储数据*/
+    void push(const SmartPtr<string> &t, int data);
+};
+
+SmartPtr<string> PoolTest::get(const int &t) {
+    auto item = map_test.find(t);
+    /*说明直接引用消除了，直接删除*/
+    if (item->second.get_delete()) {
+        printf("delete\n");
+        map_test.erase(t);
+        SmartPtr<string> null(new string("null"));
+        return null;
+    } else {
+        return item->second;
+    }
+}
+
+void PoolTest::push(const SmartPtr<string> &t, int data) {
+    auto item = map_test.find(data);
+    if (item != map_test.end()) {
+        item->second = t;
+    } else {
+        map_test.emplace(data, t);
+    }
+}
+
+void insert(PoolTest &pool_test) {
+    SmartPtr<string> ptr_1(new string("1111"));
+    SmartPtr<string> ptr_2(new string("111s"));
+    pool_test.push(ptr_1, 1);
+    pool_test.push(ptr_2, 2);
+    printf("insert end\n");
+}
+
+void get(PoolTest &pool_test, PoolTest &pool_3) {
+    printf("get data\n");
+    SmartPtr<string> ptr_1 = pool_test.get(1);
+    SmartPtr<string> ptr_2 = pool_test.get(2);
+    pool_3.push(ptr_1, 1);
+    pool_3.push(ptr_2, 2);
+    printf("插入第二个pool\n");
+}
+
 mutex whar;
 int i = 0;
 
@@ -39,16 +89,21 @@ void test_lock(int s) {
 }
 
 int main() {
-    SmartPtr<string> ptr_1(new string("1111"));
-    printf("%d\n", ptr_1.use_count());
-    SmartPtr<string> ptr_2 = ptr_1;
-    printf("%d\n", ptr_2.use_count());
-
-    map<SmartPtr<string>, int> map_test;
-    map_test.emplace(ptr_1, 2);
-    map_test.emplace(ptr_2, 3);
-
-    auto ptr_3 = map_test.find(ptr_1);
+    PoolTest pool_t = PoolTest();
+    PoolTest pool_3 = PoolTest();
+    insert(pool_t);
+    get(pool_t, pool_3);
+//    printf("what fuck %zu\n",map_test.size());
+//
+//    auto ptr_3 = map_test.find(ptr_1);
+//    if(ptr_3==map_test.end()){
+//        printf("what fuck %zu\n",map_test.size());
+//    }
+//    auto ptr_4 = map_test.find(ptr_2);
+//    if(ptr_4==map_test.end()){
+//        printf("what fuck\n");
+//    }
+//    printf("%d\n",ptr_4->second);
 //    thread t1(test_lock, 1);
 //    thread t2(test_lock, 2);
 //    t1.join();
